9cab58c6062bf57c991d9417903ea9da2328ab0c
Kevon Houghton <kevonhoughton@outlook.com>
2015-12-28 16:36:52 -0800

    Adding some basic concepts

---
+++ MainConsole/ClassLibrary1/Being.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    public abstract class Being
+    {
+        //Beings represent things one can interact with inside the game
+        //Beings share certain characteristics, here are a few basic ones
+
+        int _Strength;
+        int _Dexterity;
+        int _HealthPoints;
+
+        //Beings can also DO things, again, here are a few basic ones
+
+        //Some things are done differently between each sub-class, so you don't want to provide any implementation.
+        //They are marked as abstract 
+        public abstract bool Flee();
+
+        //some things are done the same regardless, they're just methods with default behavior.  This behavior will be used for all sub-classes, unless overridden.  \
+        //We're expecting some of this to be overridden, so we mark them as virtual. 
+        public virtual bool Attack()
+        {
+            bool retValue= false;
+            //do some logic 
+            return retValue;
+        }
+
+        //Some things we want to be the same regardless, so we don't want them to be overridden and we do not make them Virtual
+        public int TakeDamage()
+        {
+            int retValue = _HealthPoints;
+            //do some logic
+            return retValue;
+        }
+
+        //some things can only happen at the main level
+        private void Die()
+        {
+            // do some logic
+        }
+    }
+}
---
+++ MainConsole/ClassLibrary1/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class Player : Being  //we say that a Player derives from (is a) Being 
+    {
+        //Since the Flee method is Abstract, we have to implement it, or it will not compile.
+        public override bool Flee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Attack()
+        {
+            bool retValue = true;
+            //all of our players automatically hit every time, so we're going to override the base value and return our own.
+            //obviously this will not work so well in an actual implementation.  Let's figure out some logic later.
+            return retValue;
+        }
+    }
+}
